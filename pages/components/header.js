f0acf60d40647e7ad61d9e692b97cd01634f805d
24LP0532 24LP0532 <24LP0532@G401-CC0212.local>
2024-07-04 20:44:31 +0800

    urgeljlel

--- pages/components/header.js
+++ pages/components/header.js
@@ -4,12 +4,32 @@ import { GiHamburgerMenu } from "react-icons/gi";
 
 export function Header() {
     return (
-        <div className="bg-slate-200 flex justify-between h-10 text-lg pt-1.5">
-            <div className="font-semibold">{`<SS />`}</div>
-            <div className="hidden lg:block">Desktop Navigation</div>
-            <div className="hidden lg:block pt-1.5"><LuSunMedium /></div>
-            <button className="hidden lg:block bg-black text-white rounded-[10px] px-4 ">Download CV</button>
-            <div className="lg:hidden size-1.5"><GiHamburgerMenu /></div>
+        <div className="w-[1440] h-[68px] bg-slate-100 text-base">
+            <div className="w-[1280px] h-[36px] bg-slate-200 flex justify-between mx-[80px] my-[16px]"> 
+                <div className="font-semibold">
+                    {`<SS />`}
+                </div>
+                <div className="flex gap-3">
+                    <div className="hidden lg:flex gap-6">
+                        <p>About</p>
+                        <p>Work</p>
+                        <p>Testimonials</p>
+                        <p>Contact</p>
+                    </div>
+                    <div className="flex gap-6">
+                        <div className="hidden lg:flex mt-1">
+                            <LuSunMedium />
+                        </div>
+                        <button className="hidden lg:flex bg-black text-white rounded-[10px] px-4 py-px">
+                            Download CV
+                        </button>
+                    </div>
+                </div>
+                <div className="lg:hidden">
+                    <GiHamburgerMenu />
+                </div>
+
+            </div>
         </div>
     );
 }
--- pages/components/hero.js
+++ pages/components/hero.js
@@ -1,35 +1,67 @@
 import { Aboutme } from "./AboutMe";
 import { Handwaving } from "./Handwaving";
 import Image from "next/image";
+import { IoLocationOutline } from "react-icons/io5";
+import { GoDotFill } from "react-icons/go";
+import { LuGithub } from "react-icons/lu";
+import { FiTwitter } from "react-icons/fi";
+import { FiFigma } from "react-icons/fi";
+
 
 export function Hero() {
     return (
-    <>
-    <container>
-            <div className="flex items-center">
-                <Image className="shadow-2xl" src="/Pic.png" width={200} height={200} />
-            </div>
-            
-            <div className="flex px-2 ">
-            <h1 className="text-6xl font-bold">
-                Hi, I'm Sagar
-            </h1> 
-                <Handwaving />
-            </div>
-            <div className="text-xs">
-                I'm a full stack developer (React.js & Node.js)
-                with a focus on creating (and occasionally designing) 
-                exceptional digital experiences that are fast, accessible, 
-                visually appealing, and responsive. Even though I have
-                been creating web applications for over 7 years, I still
-                love it as if it was something new.
+        
+        <div className="w-[1440px] h-[360px] bg-slate-300">
+            <div className="md:flex w-[1280px] h-[360px] bg-blue-200 mx-[80px] my-[96px]">
+                <div className="h-[400px] w-[360px] relative md:order-1">
+                    <div className="h-[320px] w-[280px] absolute bg-slate-400 right-[20px] top-[20px]"></div>
+                    <Image className="h-[320px] w-[280px] absolute z-10 " src="/Pic.png" width={280} height={320} />
+                </div>
+                <div className="flex-1 ">
+                    <div className="flex px-2 m-6">
+                        <h1 className="text-6xl font-bold">
+                            Hi, I'm Sagar
+                        </h1>
+                        <Handwaving />
+                    </div>
+                    <div className="text-base mb-10 m-6">
+                        I'm a full stack developer (React.js & Node.js)
+                        with a focus on creating (and occasionally designing)
+                        exceptional digital experiences that are fast, accessible,
+                        visually appealing, and responsive. Even though I have
+                        been creating web applications for over 7 years, I still
+                        love it as if it was something new.
+                    </div>
+                    <div className="flex flex-col text-base mb-10 m-6">
+                        <div className="flex items-center gap-2">
+                            <span><IoLocationOutline /></span>
+                            <p className="mx-3">Ahmedabad, India</p>
+                        </div>
+                        <div className="flex items-center gap-2">
+                            <div className="h-2 w-2 bg-green-500 rounded-full mx-1"></div>
+                            <p className="mx-3">Availiable for new projects</p>
+                        </div>
+                    </div>
+                    <div className="flex m-6 space-x-6">
+                        <span>
+                            <LuGithub className="h-[36px] w-[36px]"/>
+                        </span>
+                        <span>
+                            <FiTwitter className="h-[36px] w-[36px]"/>
+                        </span>
+                        <span>
+                            <FiFigma className="h-[36px] w-[36px]"/>
+                        </span>
+                    </div>
+                </div>
 
-              <div><span>Logo</span> Ahmedabad, India</div>
-              <div><span>Green Dot</span> Availiable for new projects</div>
-              <div><span>GitHub</span><span>Tweeter</span><span>Figma logo</span></div>  
-            </div>   
-    </container>
-    </>
+            </div> 
+            {/* <div className="relative w-[300px] h-[300px] bg-red-300">
+                <div></div>
+                <div></div>
+            </div> */}
+        </div>
+
 
     );
 }
