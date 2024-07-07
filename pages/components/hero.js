95f5b0767176b7dab9eb389d2248e6ea0b854ac2
Galtbatzana <164704929+Galtbatzana@users.noreply.github.com>
2024-07-08 02:38:25 +0800

    hero

--- pages/components/hero.js
+++ pages/components/hero.js
@@ -12,12 +12,54 @@ export function Hero() {
     return (
         
         <div className="flex sm:w-[343px] lg:w-[1280px] bg-blue-200">
-          <div>
+          <div className="flex flex-col gap-12">
             <div className="h-[300px] w-[343px] relative">
                 <div className="h-[280px] w-[280px] absolute bg-[#E5E7EB] right-[35px] top-[30px]"></div>
                 <Image className="absolute z-10 right-[50px] top-[10px]" src="/Pic.png" width={240} height={280}/>
             </div>
-            <div>text</div>
+            <div className="bg-blue-50 w-[343px] h-[404px]">
+                <div className="flex bg-amber-100 h-auto">
+                    <h1 className="font-bold">Hi, I'm Sagar</h1>
+                    <Handwaving className="h-[36px] w-[36px]"/>              
+                </div>
+
+                <div className="text-[#4B5563] mb-12">
+                    I'm a full stack developer (React.js & Node.js)
+                    with a focus on creating (and occasionally designing)
+                    exceptional digital experiences that are fast, accessible,
+                    visually appealing, and responsive. Even though I have
+                    been creating web applications for over 7 years, I still
+                    love it as if it was something new.
+                </div>
+                <div className="mb-2">
+                    <div>
+                        <div className="flex items-center gap-2">
+                            <span><IoLocationOutline /></span>
+                            <p className="mx-3">Ahmedabad, India</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="flex items-center gap-2">
+                            <div className="h-2 w-2 bg-green-500 rounded-full mx-1"></div>
+                            <p className="mx-3">Availiable for new projects</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="flex m-6 space-x-6">
+                    <span>
+                        <LuGithub className="h-[24px] w-[24px]"/>
+                    </span>
+                    <span>
+                        <FiTwitter className="h-[24px] w-[24px]"/>
+                    </span>
+                    <span>
+                        <FiFigma className="h-[24px] w-[24px]"/>
+                    </span>
+                </div>
+
+
+            </div>
+
           </div>
         </div>
 
